5e26add0ae943f9c80f0b5d97e0b13fa5c737b06
nirmala66 <nirmalaa0606@gmail.com>
2019-07-24 10:48:26 +0700

    Intial Commit

---
+++ AssetManagement/Context/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext () : base("AssetManagement") { }
+    }
+}
